341ae5a7eab30671a04eb032fd8cd2e353e3a1d4
Mahsa Mousavi <mahsa.mousavi@dotin.ir>
2025-08-26 12:00:28 +0330

    feat (Fake Api) add fake api for TravelCoveragePackages [IO-250]

---
+++ src/1.Core/RequestResponse/TravelCoveragePackages/Queries/GetAll/GetAllTravelCoveragePackageQuery.cs
@@ -0,0 +1,7 @@
+﻿using Master.Data.Core.RequestResponse.Cities.Queries.CommanResults;
+using Zamin.Core.RequestResponse.Queries;
+
+namespace Master.Data.Core.RequestResponse.Cities.Queries.GetAll;
+public sealed class GetAllTravelCoveragePackageQuery : IQuery<List<TravelCoveragePackageItemQr>>
+{
+}
---
+++ src/3.Endpoints/API/TravelCoveragePackages/TravelCoveragePackageController.cs
@@ -0,0 +1,24 @@
+﻿using Master.Data.Core.RequestResponse.Cities.Queries.CommanResults;
+using Master.Data.Core.RequestResponse.Cities.Queries.GetAll;
+using Master.Data.Endpoints.API.Infrastructor.Attributes;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Zamin.EndPoints.Web.Controllers;
+
+namespace Master.Data.Endpoints.API.TravelCoveragePackage;
+
+[Route("api/[controller]")]
+[Tags("TravelCoveragePackage - (بسته بیمه)")]
+[ValidateTenantHeader]
+public class TravelCoveragePackageController : BaseController
+{
+    [HttpGet("GetAllTravelCoveragePackage")]
+    [AllowAnonymous]
+    public async Task<IActionResult> GetAllTravelCoveragePackage([FromQuery] GetAllTravelCoveragePackageQuery query)
+    {
+        return Ok(new[] {
+            new TravelCoveragePackageItemQr { Title = "بیمه کامل", Id = 1 },
+            new TravelCoveragePackageItemQr { Title = "خانه و مسافران", Id = 21 }
+        });
+    }
+}
