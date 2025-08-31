04b59219523555aef58febcb2818607967763a2f
Mahsa Mousavi <mahsa.mousavi@dotin.ir>
2025-08-31 14:27:30 +0330

    feat (Fake Api) add VehicleType fake api [OI-250]

--- src/3.Endpoints/API/VehicleColors/VehicleBrandController.cs
+++ src/3.Endpoints/API/VehicleColors/VehicleBrandController.cs
@@ -8,7 +8,7 @@ using Zamin.EndPoints.Web.Controllers;
 namespace Master.Data.Endpoints.API.VehicleColors;
 
 [Route("api/[controller]")]
-[Tags("VehicleColor - (رنگ خودرو)")]
+[Tags("VehicleColor - (رنگ وس..یله نقلیه)")]
 [ValidateTenantHeader]
 public class VehicleColorController : BaseController
 {
