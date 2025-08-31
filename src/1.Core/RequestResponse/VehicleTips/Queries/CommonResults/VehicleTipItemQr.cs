c70197964406eb33409daf7c624504d7c677fbef
Mahsa Mousavi <mahsa.mousavi@dotin.ir>
2025-08-31 11:47:51 +0330

    feat (Fake api) add fake api for VehicleTip [OI-250]

---
+++ src/1.Core/RequestResponse/VehicleTips/Queries/CommonResults/VehicleTipItemQr.cs
@@ -0,0 +1,7 @@
+﻿namespace Master.Data.Core.RequestResponse.VehicleTips.Queries.CommonResults;
+
+public class VehicleTipItemQr
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+}
