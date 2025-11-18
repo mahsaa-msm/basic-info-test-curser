58ae6406c656ca07e35903272408291dbd0b100e
milad bahaloo <milad.bahaloo@dotin.ir>
2025-11-18 12:48:58 +0330

    feat: add utils for generate translation script and seed translations from softwareManagementDb

---
+++ Items/Utils/TranslationSQLScriptGenerator/SimpleMultiTenantTranslationScriptGenerator.bat
@@ -0,0 +1,91 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+chcp 65001
+set /p FileName="Insert a name for your sql file (Without .sql): "
+
+echo USE [DIP_MasterData] >> "%FileName%.sql"
+echo GO >> "%FileName%.sql"
+
+set /p ApplyToAllTenants="Apply to all tenants? (yes/no): "
+
+if /I "!ApplyToAllTenants!"=="yes" (
+    echo -- Applying translations for all tenants >> "%FileName%.sql"
+) else (
+    set /p TenantName="Enter tenant name: "
+    echo -- Applying translations for tenant: !TenantName! >> "%FileName%.sql"
+    echo DECLARE @TenantId INT >> "%FileName%.sql"
+    echo DECLARE @TenantBusinessId UNIQUEIDENTIFIER >> "%FileName%.sql"
+    echo. >> "%FileName%.sql"
+    echo SELECT @TenantId = Id, @TenantBusinessId = BusinessId FROM Tenants WHERE Name = '!TenantName!' >> "%FileName%.sql"
+    echo. >> "%FileName%.sql"
+)
+
+:loop
+CALL :proccessGetTranslation
+set /p Continue="Do you have any record to add(yes/no): "
+if /I "!Continue!" NEQ "no" goto loop
+echo Scripts saved in '%FileName%.sql'.
+endlocal
+goto :eof
+
+:proccessGetTranslation
+set /p Key="Whats your 'Key': "
+set /p Value="Whats '%Key%'s 'Value': "
+
+if /I "!ApplyToAllTenants!"=="yes" (
+    for %%C in ("fa-IR" "en-US") do (
+        set Culture=%%~C
+        CALL :processForAllTenants
+    )
+) else (
+    for %%C in ("fa-IR" "en-US") do (
+        set Culture=%%~C
+        CALL :processForSingleTenant
+    )
+)
+
+goto :eof
+
+:processForAllTenants
+for /f %%i in ('powershell -Command "[guid]::NewGuid().ToString()"') do set BusinessId=%%i
+echo DECLARE @TenantCursor CURSOR >> "%FileName%.sql"
+echo DECLARE @TenantId INT >> "%FileName%.sql"
+echo DECLARE @TenantBusinessId UNIQUEIDENTIFIER >> "%FileName%.sql"
+echo. >> "%FileName%.sql"
+echo SET @TenantCursor = CURSOR FOR >> "%FileName%.sql"
+echo SELECT Id, BusinessId FROM Tenants >> "%FileName%.sql"
+echo. >> "%FileName%.sql"
+echo OPEN @TenantCursor >> "%FileName%.sql"
+echo FETCH NEXT FROM @TenantCursor INTO @TenantId, @TenantBusinessId >> "%FileName%.sql"
+echo. >> "%FileName%.sql"
+echo WHILE @@FETCH_STATUS = 0 >> "%FileName%.sql"
+echo BEGIN >> "%FileName%.sql"
+echo     IF EXISTS (SELECT * FROM [dbo].[ParrotTranslations] WHERE [Key] = N'!Key!' AND [Culture] = '!Culture!' AND [TenantId] = @TenantId) >> "%FileName%.sql"
+echo     BEGIN >> "%FileName%.sql"
+echo         UPDATE [dbo].[ParrotTranslations] SET [Value] = N'!Value!', [ModifiedByUserId] = '0', [ModifiedDateTime] = GETDATE(), [BusinessId] = '!BusinessId!', [TenantBusinessId] = @TenantBusinessId WHERE [Key] = N'!Key!' AND [Culture] = '!Culture!' AND [TenantId] = @TenantId >> "%FileName%.sql"
+echo     END >> "%FileName%.sql"
+echo     ELSE >> "%FileName%.sql"
+echo     BEGIN >> "%FileName%.sql"
+echo         INSERT INTO [dbo].[ParrotTranslations] ([Key], [Value], [Culture], [CreatedByUserId], [CreatedDateTime], [BusinessId], [TenantId], [TenantBusinessId]) VALUES (N'!Key!', N'!Value!', '!Culture!', '0', GETDATE(), '!BusinessId!', @TenantId, @TenantBusinessId) >> "%FileName%.sql"
+echo     END >> "%FileName%.sql"
+echo     FETCH NEXT FROM @TenantCursor INTO @TenantId, @TenantBusinessId >> "%FileName%.sql"
+echo END >> "%FileName%.sql"
+echo. >> "%FileName%.sql"
+echo CLOSE @TenantCursor >> "%FileName%.sql"
+echo DEALLOCATE @TenantCursor >> "%FileName%.sql"
+echo GO >> "%FileName%.sql"
+goto :eof
+
+:processForSingleTenant
+for /f %%i in ('powershell -Command "[guid]::NewGuid().ToString()"') do set BusinessId=%%i
+echo IF EXISTS (SELECT * FROM [dbo].[ParrotTranslations] WHERE [Key] = N'!Key!' AND [Culture] = '!Culture!' AND [TenantId] = @TenantId) >> "%FileName%.sql"
+echo BEGIN >> "%FileName%.sql"
+echo     UPDATE [dbo].[ParrotTranslations] SET [Value] = N'!Value!', [ModifiedByUserId] = '0', [ModifiedDateTime] = GETDATE(), [BusinessId] = '!BusinessId!', [TenantBusinessId] = @TenantBusinessId WHERE [Key] = N'!Key!' AND [Culture] = '!Culture!' AND [TenantId] = @TenantId >> "%FileName%.sql"
+echo END >> "%FileName%.sql"
+echo ELSE >> "%FileName%.sql"
+echo BEGIN >> "%FileName%.sql"
+echo     INSERT INTO [dbo].[ParrotTranslations] ([Key], [Value], [Culture], [CreatedByUserId], [CreatedDateTime], [BusinessId], [TenantId], [TenantBusinessId]) VALUES (N'!Key!', N'!Value!', '!Culture!', '0', GETDATE(), '!BusinessId!', @TenantId, @TenantBusinessId) >> "%FileName%.sql"
+echo END >> "%FileName%.sql"
+echo GO >> "%FileName%.sql"
+goto :eof
