3bf19d97a9df027232a02249ed2ab087bc9f7437
Mahsa Mousavi <mahsa.mousavi@dotin.ir>
2026-01-19 16:03:44 +0330

    fix (InsuranceType) add InsuranceType in host [OI-700]

---
+++ src/3.Endpoints/HostedService/Jobs/InsuranceTypes/FetchMultiTenantInsuranceTypesFromCoreJob.cs
@@ -0,0 +1,17 @@
+﻿using Master.Data.Core.RequestResponse.InsuranceTypes.Commands.Fetch;
+using Master.Data.Endpoints.HostedService.Infrastructures.Services.BackgroundJob;
+using Master.Data.Endpoints.HostedService.Infrastructures.Services.BackgroundJob.Models;
+using Microsoft.Extensions.Options;
+
+namespace Master.Data.Endpoints.HostedService.Jobs.InsuranceTypes;
+
+public sealed class FetchMultiTenantInsuranceTypesFromCoreJob :
+    BaseBackgroundJob<FetchMultiTenantInsuranceTypesFromSourceCommand, FetchMultiTenantInsuranceTypesFromCoreJob>
+{
+    public FetchMultiTenantInsuranceTypesFromCoreJob(IServiceScopeFactory serviceScopeFactory,
+                                                ILogger<FetchMultiTenantInsuranceTypesFromCoreJob> logger,
+                                                IOptions<JobScheduleOption> jobSchedule)
+        : base(serviceScopeFactory, logger, jobSchedule)
+    {
+    }
+}
