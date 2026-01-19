c21de781f5985bad5c5217cd9470adedf584fbae
Mahsa Mousavi <mahsa.mousavi@dotin.ir>
2026-01-19 15:22:29 +0330

    feat (AddInsuranceType ) add migraion scripts [OI-700]

---
+++ src/2.Infra/Data/Sql.Commands/Migrations/20260119114756_AddInsuranceType.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Master.Data.Infra.Data.Sql.Commands.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInsuranceType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "InsuranceTypes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    DisplayTitle = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    CoreId = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Priority = table.Column<long>(type: "bigint", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedByUserId = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    CreatedDateTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedByUserId = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    ModifiedDateTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    BusinessId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TenantId = table.Column<long>(type: "bigint", nullable: false),
+                    TenantBusinessId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InsuranceTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InsuranceTypes_BusinessId",
+                table: "InsuranceTypes",
+                column: "BusinessId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InsuranceTypes_TenantId_CoreId",
+                table: "InsuranceTypes",
+                columns: new[] { "TenantId", "CoreId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InsuranceTypes");
+        }
+    }
+}
