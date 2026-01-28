774421a8dc7a3cb89aa5ebafb3d12cf9146a1aac
milad bahaloo <milad.bahaloo@dotin.ir>
2026-01-28 14:39:50 +0330

    fix: city-province-country query relation null assertation [OI-744]

--- src/2.Infra/Data/Sql.Queries/InsuranceUnits/Configs/InsuranceUnitConfig.cs
+++ src/2.Infra/Data/Sql.Queries/InsuranceUnits/Configs/InsuranceUnitConfig.cs
@@ -1,5 +1,4 @@
-﻿using Master.Data.Infra.Data.Sql.Queries.Cities.Entities;
-using Master.Data.Infra.Data.Sql.Queries.InsuranceUnits.Entities;
+﻿using Master.Data.Infra.Data.Sql.Queries.InsuranceUnits.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -15,9 +14,11 @@ public sealed class InsuranceUnitConfig : IEntityTypeConfiguration<InsuranceUnit
         .HasForeignKey(c => c.TenantId);
 
         builder
-        .HasOne<City>()
+        .HasOne(c => c.City)
         .WithMany()
         .HasPrincipalKey(c => new { c.TenantId, c.CoreId }) // کلید اصلی ترکیبی
-        .HasForeignKey(p => new { p.TenantId, p.CityCoreId }); // کلید خارجی ترکیبی
+        .HasForeignKey(p => new { p.TenantId, p.CityCoreId }) // کلید خارجی ترکیبی
+        .IsRequired(false)
+        .OnDelete(DeleteBehavior.NoAction);
     }
 }
