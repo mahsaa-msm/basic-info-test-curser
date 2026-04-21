4c117641cbaa35aa995b2dc482376df8e7699b61
Mahsa Mousavi <mahsa.mousavi@dotin.ir>
2026-04-21 11:31:34 +0330

    feat (Priority) update Priority for city and province [OI-969]

---
+++ Items/Scripts/Version 2.0.2/01-updatePriorityScript.sql
@@ -0,0 +1,45 @@
+﻿USE DIP_MasterData
+GO
+
+WITH SortedCities AS
+(
+    SELECT 
+        Id,
+        ROW_NUMBER() OVER (
+            ORDER BY 
+                CASE 
+                    WHEN Title COLLATE Persian_100_CI_AS LIKE N'%[آ-ی]%' THEN 1 
+                    ELSE 2 
+                END,
+                
+                Title COLLATE Persian_100_CI_AS
+        ) AS NewPriority
+    FROM [DIP_MasterData].[dbo].[Cities]
+)
+UPDATE C
+SET C.Priority = S.NewPriority
+FROM [DIP_MasterData].[dbo].[Cities] C
+INNER JOIN SortedCities S ON C.Id = S.Id;
+
+
+Go
+
+WITH SortedProvinces AS
+(
+    SELECT 
+        Id,
+        ROW_NUMBER() OVER (
+            ORDER BY 
+                CASE 
+                    WHEN Title COLLATE Persian_100_CI_AS LIKE N'%[آ-ی]%' THEN 1 
+                    ELSE 2 
+                END,
+                
+                Title COLLATE Persian_100_CI_AS
+        ) AS NewPriority
+    FROM [DIP_MasterData].[dbo].Provinces
+)
+UPDATE C
+SET C.Priority = S.NewPriority
+FROM [DIP_MasterData].[dbo].Provinces C
+INNER JOIN SortedProvinces S ON C.Id = S.Id;
